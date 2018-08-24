b0ebc027fe5ae8f3134e232ef0999a724eeed6ff
rizidorio <rferreira013@hotmail.com>
2018-08-24 10:41:52 -0300

    Camada xadrez e impressao tabuleiro

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,9 +1,13 @@
 package application;
 
+import xadrez.PartidaXadrez;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		PartidaXadrez partida = new PartidaXadrez();
+		UI.imprimirTabuleiro(partida.getPecas());
 
 	}
 
